5482e96cca88bae3744b1dc710ec0fc5cb89cfb3
chiostro <notoand@gmail.com>
2025-09-06 10:15:26 +0200

    Update signpdf.sh

--- signpdf.sh
+++ signpdf.sh
@@ -22,18 +22,15 @@ coord=${6:-"+130+350"}
 #Check files
 test ! -f $pdfname && echo "PDF notte fonda" && exit 1
 test ! -f $signpng && echo "PNG notte fonda" && exit 1
-#Workin area cration
-swdir=`pwd`/
-wkpath=${swdir}tmp/
-test ! -d $wkpath &&  mkdir $wkpath
+
 #Split the document in n pages
 i=1
 while [ $i -le $numpage ]
 do
-    pdftk $pdfname  cat $i  output ${wkpath}pag$i.pdf
+    pdftk $pdfname  cat $i  output pag$i.pdf
     i=`expr $i + 1`
 done
-cd $wkpath
+
 #Transforn pdf in png
 pdftoppm -png pag${pagetosign}.pdf page${pagetosign}
 
@@ -59,7 +56,5 @@ do
     i=`expr $i + 1`
 done
 #Print pdf with new name
-pdftk $command cat output ${swdir}signed_${pdfname}
-#Remove working area files
-shred -vuz -n 3 *
-cd ..
+pdftk $command cat output signed_${pdfname}
+
