8c95c5dee9f11298fe1de33ea5b74500f6bfb6bc
chiostro <notoand@gmail.com>
2025-09-07 13:57:32 +0200

    Update cutCaterinaIIpng.sh

--- cutCaterinaIIpng.sh
+++ cutCaterinaIIpng.sh
@@ -1,5 +1,5 @@
 # Using the command "identify" CaterinaII.png, you get info like how many pixels in the pic
-# Using command "magick" your_pic.png, you cut frame starting from position of pixels +450+280, getting a  rectangle of lenght hight 420x195  
+# Using command "magick" your_pic.png, starting from position of pixels +450+280, you crop a rectangle of lenght hight 420x195  
 
 magick CaterinaII.png -crop 420x195+450+280 +repage left_half.png
 
